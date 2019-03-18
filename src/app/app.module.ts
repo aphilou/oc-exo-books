86d7aa4bade50489974ae246446f1ee570c8488c
Philippe Yo <poub1@neuf.fr>
2019-03-18 10:35:09 +0100

    Add a few components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { BookListViewComponent } from './book-list-view/book-list-view.component';
+import { BookViewComponent } from './book-list-view/book-view/book-view.component';
+import { BookFormComponent } from './book-list-view/book-form/book-form.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    SigninComponent,
+    BookListViewComponent,
+    BookViewComponent,
+    BookFormComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/book-list-view/book-list-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-book-list-view',
+  templateUrl: './book-list-view.component.html',
+  styleUrls: ['./book-list-view.component.scss']
+})
+export class BookListViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
