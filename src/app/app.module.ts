e74ee1a46ac39fe3213454c20756394f347fa62f
Philippe Yo <poub1@neuf.fr>
2019-03-18 12:52:14 +0100

    Créer les menus

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { SignupComponent } from './auth/signup/signup.component';
@@ -8,6 +11,21 @@ import { BookListViewComponent } from './book-list-view/book-list-view.component
 import { BookViewComponent } from './book-list-view/book-view/book-view.component';
 import { BookFormComponent } from './book-list-view/book-form/book-form.component';
 import { HeaderComponent } from './header/header.component';
+import { AuthGuardService } from './services/auth-guard.service';
+import { BooksService } from './services/books.service';
+
+
+const routes: Routes = [
+  { path: 'auth/signup', component: SignupComponent },
+  { path: 'auth/signin', component: SigninComponent },
+  { path: 'books', component: BookListViewComponent },
+  { path: 'books/create', component: BookFormComponent },
+  { path: 'books/detail/:id', component: BookViewComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'auth/signup' }
+];
+
+export const appRouting = RouterModule.forRoot(routes);
+
 
 @NgModule({
   declarations: [
@@ -20,9 +38,16 @@ import { HeaderComponent } from './header/header.component';
     HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [
+    AuthGuardService,
+    BooksService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
