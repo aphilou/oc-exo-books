1b4b4237ac2a8cdde7b98bedf4ea65d16d207e3d
Philippe Yo <poub1@neuf.fr>
2019-03-20 01:17:46 +0100

    storage

--- src/app/book-list-view/book-form/book-form.component.ts
+++ src/app/book-list-view/book-form/book-form.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { BooksService } from 'src/app/services/books.service';
-import { Router } from '@angular/router';
+import { Router, Event } from '@angular/router';
 import { Book } from 'src/app/models/book.model';
 
 @Component({
@@ -12,6 +12,9 @@ import { Book } from 'src/app/models/book.model';
 export class BookFormComponent implements OnInit {
 
   bookForm: FormGroup;
+  fileUploading = false;
+  fileUrl: string;
+  fileUploaded = false;
 
   constructor(private formBuilder: FormBuilder,
               private bookService: BooksService,
@@ -32,7 +35,25 @@ export class BookFormComponent implements OnInit {
     const title = this.bookForm.get('title').value;
     const author = this.bookForm.get('author').value;
     const bookToAdd = new Book(title, author);
+    if (this.fileUrl && this.fileUrl !== '') {
+      bookToAdd.photo = this.fileUrl;
+    }
     this.bookService.createBook(bookToAdd);
     this.router.navigate([ '/books' ]);
+  }
+
+  onUploadFile(file: File) {
+    this.fileUploading = true;
+    this.bookService.uploadFile(file).then(
+      (url: string) => {
+        this.fileUrl = url;
+        this.fileUploaded = true;
+        this.fileUploading = false;
+      }
+    );
+  }
+
+  onEvent(event: any) {
+    this.onUploadFile(event.target.files[0]);
   }
 }
--- src/app/services/books.service.ts
+++ src/app/services/books.service.ts
@@ -53,6 +53,18 @@ export class BooksService {
   }
 
   removeBook(book: Book) {
+    if (book.photo) {
+      const storageRef = firebase.storage().refFromURL(book.photo);
+      storageRef.delete().then(
+        () => {
+          console.log('Photo supprimée !');
+        }
+      ).catch (
+        (error) => {
+          console.error('Fichier non trouvé : ', error);
+        }
+      );
+    }
     const bookIndexToRemove = this.books.findIndex(
       (item) => {
         if (item === book) {
@@ -63,5 +75,28 @@ export class BooksService {
     this.books.splice(bookIndexToRemove, 1);
     this.saveBooks();
     this.emitBooks();
+  }
+
+  uploadFile(file: File) {
+    return new Promise(
+      (resolve, reject) => {
+        const almostUniqueFileName = Date.now().toString();
+        const upload = firebase.storage().ref()
+          .child('images/' + almostUniqueFileName + file.name)
+          .put(file);
+        upload.on(firebase.storage.TaskEvent.STATE_CHANGED,
+          () => {
+            console.log('Chargement en cours...');
+          },
+          (error: any) => {
+            console.error('Erreur de chargement: ', error);
+            reject();
+          },
+          () => {
+            resolve(upload.snapshot.ref.getDownloadURL());
+          }
+        );
+      }
+    );
   }
 }
